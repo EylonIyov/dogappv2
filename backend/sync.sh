98ad71c75902f29ad34eb20ff8053164342e2bd5
Eylon Iyov <eylon.iyov@post.runi.ac.il>
2025-08-24 19:18:31 +0300

    deleted uneeded files

--- backend/sync.sh
+++ backend/sync.sh
@@ -1,4 +1,4 @@
-rsync -avz --exclude 'node_modules' --exclude '.env' --exclude '.git' --exclude 'package-lock.json' --exclude 'sync.sh' \
+rsync -avz --exclude 'node_modules' --exclude '.git' --exclude 'package-lock.json' --exclude 'sync.sh' \
 -e "ssh -i ~/Desktop/.ssh/israel-EylonsMacbook.pem" \
 . ubuntu@ec2-13-48-57-9.eu-north-1.compute.amazonaws.com:~/app
 
